a761ccebf4f3ed7ff15f0ed7959ac46587be7f03
taty-ko <hrkolesnik@gmail.com>
2021-10-24 22:33:22 +0300

    Uploaded the test project.

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+var mybutton = document.getElementById("back-to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
